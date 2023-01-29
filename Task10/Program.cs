d17a3421e0daca8d85f8bb18b0c91f32169e0a5b
E. Khaustov <hrust656@gmail.com>
2023-01-29 19:40:56 +1000

    task 10 is done (version 2)

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,6 +1,12 @@
 ﻿// Задача 10. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа
 Console.Clear();
-int n = new Random().Next(100, 1000);
-Console.WriteLine(n);
-int n2 = (n / 10) % 10;
-Console.WriteLine($"{n2}");
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Введенное число: {n}");
+if (n / 10 == 0)
+    Console.WriteLine($"Число {n} состоит из одной цифры");
+else
+{
+    int n2 = (n / 10) % 10;
+    Console.WriteLine($"Вторая цифра числа: {n2}");
+}
